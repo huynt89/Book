d0a4326c8b867d9364e7177f071b2324dc3e5fe5
huynt4 <tuanhuycntt@gmail.com>
2025-11-22 20:44:25 +0700

    Update comic_data.js

--- comic_data.js
+++ comic_data.js
@@ -9,61 +9,26 @@ const COMIC_DATA_JSON = [
 		upload_date:  "2025-11-22",
 		description:  "Mèo máy Doraemon và Nobita",
 		cover:  "Test1.jpg"
-	},
+	},s
 	{
-		title:  "Test1",
-		folder:  "Test1",
-		upload_date:  "2025-11-16",
-		description:  "Test1",
-		cover:  "Test1.jpg"
-	},
-	{
-		title:  "Test2",
-		folder:  "Test2",
-		upload_date:  "2025-11-16",
-		description:  "Test2",
-		cover:  "Test2.jpg"
-	},
-	{
-		title:  "Test3",
-		folder:  "Test3",
-		upload_date:  "2025-11-16",
-		description:  "Test3",
-		cover:  "Test3.jpg"
-	},
-	{
-		title:  "Test4",
-		folder:  "Test4",
-		upload_date:  "2025-11-16",
-		description:  "Test4",
-		cover:  "Test4.jpg"
-	},
-	{
-		title:  "Test5",
-		folder:  "Test5",
-		upload_date:  "2025-11-16",
-		description:  "Test5",
-		cover:  "Test5.jpg"
-	},
-	{
-		title:  "Truyá»n 7 ViÃªn Ngá»c Rá»ng",
+		title:  "Truyện 7 Viên Ngọc Rồng",
 		folder:  "7vienngocrong",
 		upload_date:  "2025-11-16",
-		description:  "Truyá»n 7 viÃªn ngá»c rá»ng",
+		description:  "Truyện 7 viên ngọc rồng",
 		cover:  "7vienngocrong_cover.jpg"
 	},
 	{
-		title:  "Truyá»n Conan",
+		title:  "Truyện Conan",
 		folder:  "Conan",
 		upload_date:  "2025-11-16",
-		description:  "Truyá»n trinh thÃ¡m Conan",
+		description:  "Truyện trinh thám Conan",
 		cover:  "Conan_cover.jpg"
 	},
 	{
-		title:  "Truyá»n Yugi-Oh",
+		title:  "Truyện Yugi-Oh",
 		folder:  "YugiOh",
 		upload_date:  "2025-11-16",
-		description:  "Vua TrÃ² ChÆ¡i Yugi-Oh",
+		description:  "Vua Trò Chơi Yugi-Oh",
 		cover:  "YugiOh_cover.jpg"
 	}
 ];
