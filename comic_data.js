3829ce23f14018f3c2bffd7fc17d1eaab8ea8eab
Nông Tuấn Huy <tuanhuycntt@gmail.com>
2025-11-22 20:23:49 +0700

    feat: Thêm truyện: Doraemon (qua Web Admin)

--- comic_data.js
+++ comic_data.js
@@ -3,68 +3,67 @@
 // Lưu ý: Đường dẫn ảnh bìa đã được đổi thành thư mục 'cover/' 
 // Bạn chỉ cần điền tên file ảnh ở đây (ví dụ: 'YugiOh_cover.jpg')
 const COMIC_DATA_JSON = [
-		{
-				"title": "Truyện Yugi-Oh",
-				"folder": "YugiOh",
-				"upload_date": "2025-11-16",
-				"description": "Vua Trò Chơi Yugi-Oh",
-				"cover": "YugiOh_cover.jpg" // Đã thay đổi đường dẫn
-		},
-		
-		{
-				"title": "Truyện Conan",
-				"folder": "Conan",
-				"upload_date": "2025-11-16",
-				"description": "Truyện trinh thám Conan",
-				"cover": "Conan_cover.jpg" // Đã thay đổi đường dẫn
-		},
-		
-		{
-				"title": "Truyện 7 Viên Ngọc Rồng",
-				"folder": "7vienngocrong",
-				"upload_date": "2025-11-16",
-				"description": "Truyện 7 viên ngọc rồng",
-				"cover": "7vienngocrong_cover.jpg" // Đã thay đổi đường dẫn
-		},
-		
-				{
-				"title": "Test1",
-				"folder": "Test1",
-				"upload_date": "2025-11-16",
-				"description": "Test1",
-				"cover": "Test1.jpg"
-		},
-		
-		{
-				"title": "Test2",
-				"folder": "Test2",
-				"upload_date": "2025-11-16",
-				"description": "Test2",
-				"cover": "Test2.jpg"
-		},
-		
-		{
-				"title": "Test3",
-				"folder": "Test3",
-				"upload_date": "2025-11-16",
-				"description": "Test3",
-				"cover": "Test3.jpg"
-		},
-		
-		{
-				"title": "Test4",
-				"folder": "Test4",
-				"upload_date": "2025-11-16",
-				"description": "Test4",
-				"cover": "Test4.jpg"
-		},
-		
-		{
-				"title": "Test5",
-				"folder": "Test5",
-				"upload_date": "2025-11-16",
-				"description": "Test5",
-				"cover": "Test5.jpg"
-		},
-    // Thêm các truyện khác vào đây
+	{
+		title:  "Doraemon",
+		folder:  "Doraemon",
+		upload_date:  "2025-11-22",
+		description:  "Mèo máy Doraemon và Nobita",
+		cover:  "Test1"
+	},
+	{
+		title:  "Test1",
+		folder:  "Test1",
+		upload_date:  "2025-11-16",
+		description:  "Test1",
+		cover:  "Test1.jpg"
+	},
+	{
+		title:  "Test2",
+		folder:  "Test2",
+		upload_date:  "2025-11-16",
+		description:  "Test2",
+		cover:  "Test2.jpg"
+	},
+	{
+		title:  "Test3",
+		folder:  "Test3",
+		upload_date:  "2025-11-16",
+		description:  "Test3",
+		cover:  "Test3.jpg"
+	},
+	{
+		title:  "Test4",
+		folder:  "Test4",
+		upload_date:  "2025-11-16",
+		description:  "Test4",
+		cover:  "Test4.jpg"
+	},
+	{
+		title:  "Test5",
+		folder:  "Test5",
+		upload_date:  "2025-11-16",
+		description:  "Test5",
+		cover:  "Test5.jpg"
+	},
+	{
+		title:  "Truyá»n 7 ViÃªn Ngá»c Rá»ng",
+		folder:  "7vienngocrong",
+		upload_date:  "2025-11-16",
+		description:  "Truyá»n 7 viÃªn ngá»c rá»ng",
+		cover:  "7vienngocrong_cover.jpg"
+	},
+	{
+		title:  "Truyá»n Conan",
+		folder:  "Conan",
+		upload_date:  "2025-11-16",
+		description:  "Truyá»n trinh thÃ¡m Conan",
+		cover:  "Conan_cover.jpg"
+	},
+	{
+		title:  "Truyá»n Yugi-Oh",
+		folder:  "YugiOh",
+		upload_date:  "2025-11-16",
+		description:  "Vua TrÃ² ChÆ¡i Yugi-Oh",
+		cover:  "YugiOh_cover.jpg"
+	}
 ];
